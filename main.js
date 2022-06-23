3c4aa353e73ed5bb299f6aac4addd5a5f5f76ac6
Ezeh Racheal <82085240+Rachy222@users.noreply.github.com>
2022-06-23 10:15:30 +0100

    first commit

--- main.js
+++ main.js
@@ -9,4 +9,4 @@ let u = a / b;
 alert("Sum of " + a + " and " + b + " is : " + x );
 alert("Difference of " + a + " and " + b + " is : " + y );
 alert("Product of " + a + " and " + b + " is : " + z );
-alert("Qouitent of " + a + " and " + b + " is : " + u );
+alert("Quoitent of " + a + " and " + b + " is : " + u );
